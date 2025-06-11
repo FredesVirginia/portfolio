157a8f3ac7619dae2d4f8f7359bb0fa3b567c2eb
Fredes Virginia <William-Champion@outlook.com>
2025-06-10 23:31:45 -0300

    w

--- src/componets/Proyectos.tsx
+++ src/componets/Proyectos.tsx
@@ -8,11 +8,28 @@ export default function Proyectos() {
           <span className="block h-1 my-2  bg-sky-400 w-50"></span>
         </div>
 
-        <div>
-            <div className="w-90 h-96 rounded-2xl border-l-10 border-cyan-300 bg-red-400">1</div>
-            <div></div>
-            <div></div>
-            <div></div>
+        <div className="flex  flex-wrap gap-6 justify-center">
+            <div className="w-90 h-96 rounded-2xl border-l-10 border-cyan-300 bg-gray-200 shadow-2xl shadow-cyan-300">
+              <p className="text-center text-[18px] pt-4 text-cyan-400 font-bold">
+                Oxford Gear Kids
+              </p>
+              
+              </div>
+            <div className="w-90 h-96 rounded-2xl border-l-10 border-cyan-300 bg-gray-200 shadow-2xl shadow-cyan-300">
+               <p className="text-center text-[18px] pt-4 text-cyan-400 font-bold">
+                Schools
+              </p>
+            </div>
+             <div className="w-90 h-96 rounded-2xl border-l-10 border-cyan-300 bg-gray-200 shadow-2xl shadow-cyan-300">
+               <p className="text-center text-[18px] pt-4 text-cyan-400 font-bold">
+                Peps
+              </p>
+             </div>
+              <div className="w-90 h-96 rounded-2xl border-l-10 border-cyan-300 bg-gray-200 shadow-2xl shadow-cyan-300">
+                 <p className="text-center text-[18px] pt-4 text-cyan-400 font-bold">
+                NovaTrend Shop
+              </p>
+              </div>
         </div>
     </div>
   )
